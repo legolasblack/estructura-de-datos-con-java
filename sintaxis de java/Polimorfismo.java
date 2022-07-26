1cdf6f0f40d52b614039c494ef04ccf1efd70abe
legolasblack <legolas.black.ds@gmail.com>
2022-07-25 22:14:08 -0500

    Archivo polimorfismo merged

--- sintaxis de java/Polimorfismo.java
+++ sintaxis de java/Polimorfismo.java
@@ -34,9 +34,7 @@
  * en la segunda manera de declararlo:
  * Animal leon=new Leon();
  * se puede acceder a los metodos y atributos de la clase padre y a los metodos
-<<<<<<< HEAD
- * sobresescritos del hijo
-=======
+
  * que sobrescribes en la clase hijo de esta manera acceder a las dos clases sin 
  * poder acceder a los atributos de la clase hijo solo los sobrescritos.
 >>>>>>> 6536b00a7baea00776f268e649ad7afb56f907c5
@@ -52,28 +50,22 @@
 public class Polimorfismo {
     public static void main(String[] args) {
         Animal leon = new Animal();
-<<<<<<< HEAD
-        Perro perro = new Perro();
-        Gato gato = new Gato();
-=======
+
         Animal perro = new Perro();
         Gato gato = new Gato();
         Perro p2 = new Perro();
->>>>>>> 6536b00a7baea00776f268e649ad7afb56f907c5
+
 
         // g.Make_Sound();
         // p.Make_Sound();
         // leon.Make_Sound();
         // p2.Make_Sound();
-<<<<<<< HEAD
-        perro.Make_Sound();
-        leon.Make_Sound();
-=======
+
         leon.Make_Sound();
         perro.Name_animal();
         perro.Make_Sound();
 
->>>>>>> 6536b00a7baea00776f268e649ad7afb56f907c5
+
         gato.Make_Sound();
 
     }
@@ -85,20 +77,14 @@ class Animal {
     public void Make_Sound() {
         System.out.println("grrrr");
     }
-<<<<<<< HEAD
 
-    public void Name_animal() {
-        System.out.println("Soy un animal");
-    }
-}
-=======
 
     public void Name_animal() {
         System.out.println("Soy un animal");
     }
 }
 
->>>>>>> 6536b00a7baea00776f268e649ad7afb56f907c5
+
 class Perro extends Animal {
 
     public void Make_Sound() {
@@ -115,7 +101,10 @@ class Gato extends Animal {
     public void Make_Sound() {
         System.out.println("miau");
     }
-<<<<<<< HEAD
+    public void Name_gato(){
+        System.out.println("Soy un gato");
+    }
+
 }
 
 /*
@@ -328,10 +317,3 @@ class Gato extends Animal {
  * }
  * }
  */
-=======
-
-    public void Name_gato() {
-        System.out.println("Soy un gato");
-    }
-}
->>>>>>> 6536b00a7baea00776f268e649ad7afb56f907c5
